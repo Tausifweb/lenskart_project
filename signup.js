d7026272aa6054e64e0fa94564fb0d5aa36b9d13
Sushanth PS <35593841+sushanthps2@users.noreply.github.com>
2021-07-23 11:06:07 +0530

    Add files via upload

---
+++ signup.js
@@ -0,0 +1,23 @@
+let signUp = document.getElementById("sign-up");
+let signIn = document.getElementById("sign-in");
+var elements = document.getElementsByTagName("*");
+
+function signup() {
+    signUp.style.display = "flex";
+    signIn.style.display = "none";
+}
+
+function signin() {
+    signUp.style.display = "none";
+    signIn.style.display = "flex";
+}
+
+function closeOption() {
+    signUp.style.display = "none";
+    signIn.style.display = "none";
+}
+
+
+setInterval(function () {
+    console.log(document.body.scrollTop);
+}, 2000);
