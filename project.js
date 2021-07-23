02a507f73410d7925c3d905a9a67af459f66f089
Anuj Sharma <anuujsharma96@gmail.com>
2021-07-23 10:54:40 +0530

    slider change

--- project.js
+++ project.js
@@ -41,3 +41,100 @@ setInterval(() => {
     i = 0;
   }
 }, 3000);
+///////////////////////////////////
+
+("use strict");
+
+productScroll();
+
+function productScroll() {
+  let slider = document.getElementById("slider");
+  let next = document.getElementsByClassName("pro-next");
+  let prev = document.getElementsByClassName("pro-prev");
+  let slide = document.getElementById("slide");
+  let item = document.getElementById("slide");
+
+  for (let i = 0; i < next.length; i++) {
+    //refer elements by class name
+
+    let position = 0; //slider postion
+
+    prev[i].addEventListener("click", function () {
+      //click previos button
+      if (position > 0) {
+        //avoid slide left beyond the first item
+        position -= 1;
+        translateX(position); //translate items
+      }
+    });
+
+    next[i].addEventListener("click", function () {
+      if (position >= 0 && position < hiddenItems()) {
+        //avoid slide right beyond the last item
+        position += 1;
+        translateX(position); //translate items
+      }
+    });
+  }
+
+  function hiddenItems() {
+    //get hidden items
+    let items = getCount(item, false);
+    let visibleItems = slider.offsetWidth / 210;
+    return items - Math.ceil(visibleItems);
+  }
+}
+
+function translateX(position) {
+  //translate items
+  slide.style.left = position * -210 + "px";
+}
+
+function getCount(parent, getChildrensChildren) {
+  //count no of items
+  let relevantChildren = 0;
+  let children = parent.childNodes.length;
+  for (let i = 0; i < children; i++) {
+    if (parent.childNodes[i].nodeType != 4) {
+      if (getChildrensChildren)
+        relevantChildren += getCount(parent.childNodes[i], true);
+      relevantChildren++;
+    }
+  }
+  return relevantChildren;
+}
+
+////////////////
+
+/// 3 iamge slideing start here
+const wrapper = document.getElementsByClassName("wrapper");
+const button = document.getElementById("click");
+const button2 = document.getElementById("click2");
+
+button.addEventListener("click", clicked);
+button2.addEventListener("click", clicked2);
+let scroll = 0;
+
+wrapper[0].addEventListener("scroll", function (event) {
+  scroll = wrapper[0].scrollLeft;
+});
+
+function clicked() {
+  scroll = scroll += 500;
+  wrapper[0].scrollTo({
+    left: scroll,
+    behavior: "smooth",
+  });
+  scroll = wrapper[0].scrollLeft + 50;
+  console.log(wrapper[0].scrollLeft);
+}
+
+function clicked2() {
+  scroll = scroll -= 500;
+  wrapper[0].scrollTo({
+    left: scroll,
+    behavior: "smooth",
+  });
+  scroll = wrapper[0].scrollLeft + 50;
+  console.log(wrapper[0].scrollLeft);
+}
